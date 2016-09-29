06ea0b470061476c9c4fbfd3dd7506707287f68c
xiashihua <shihuaxia@sohu-inc.com>
2016-09-29 12:08:42 +0800

    test database

--- agent.js
+++ agent.js
@@ -1,12 +1,12 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '909005',
-  database : 'currency'
+  host: 'localhost',
+  user: 'root',
+  password: '909005',
+  database: 'currency'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
@@ -15,7 +15,8 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
-connection.query('show databases',function(err,rows){
-  console.log(err);
-  console.log(rows);
+connection.query('show databases', function (err, rows, fields) {
+  console.log(JSON.stringify(rows));
+  console.log(rows[1].Database);
+  console.log(fields);
 });
