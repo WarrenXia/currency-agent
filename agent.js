aa4c0b5e6f6220a086d53c4c692911332dab7634
xiashihua <shihuaxia@sohu-inc.com>
2016-11-24 16:41:06 +0800

    agent

--- agent.js
+++ agent.js
@@ -1,22 +1,29 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+"use strict";
+import mysql from 'mysql';
+let mysqlConfig = {
   host: 'localhost',
   user: 'root',
-  password: '909005',
-  database: 'currency'
+  password: '909005'
+};
+const initDatabase = mysql.createConnection(mysqlConfig);
+
+initDatabase.connect();
+// 创建数据库
+initDatabase.query('create database if not exists CURRENCY');
+initDatabase.end();
+
+mysqlConfig['database'] = 'CURRENCY';
+
+const connection = mysql.createConnection(mysqlConfig);
+connection.connect();
+// 创建数据表
+connection.query('create table if not exists CURRENCY (id int,name varchar(255),country varchar(255),timestamp timestamp,date date,time time,spotin varchar(255),cashin varchar(255),spotout varchar(255),cashout varchar(255),middleprice varchar(255) )', function (err, rows, fields) {
+  // console.log(err);
+  // console.log(rows);
+  // console.log(fields);
 });
-
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-
-  console.log('connected as id ' + connection.threadId);
-});
-
-connection.query('show databases', function (err, rows, fields) {
-  console.log(JSON.stringify(rows));
-  console.log(rows[1].Database);
+connection.query('select * from CURRENCY', (err, rows, fields) => {
+  // console.log(err);
+  // console.log(rows);
   console.log(fields);
 });
