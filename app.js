233eca475748b600ca28a241224de670368abc98
xiashihua <xsh613@gmail.com>
2016-11-29 15:23:35 +0800

    agent

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const createDatabase = () => {
 };
 const createTable = () => {
   return new Promise(function (resolve, reject) {
-    connection.query('create table if not exists CURRENCY.CURRENCY (id int,name varchar(255),area varchar(255),symbol varchar(255),timestamp bigint(13),date date,time time,spotin varchar(255),cashin varchar(255),spotout varchar(255),cashout varchar(255),middleprice varchar(255) )', function (error, results, fileds) {
+    connection.query('create table if not exists CURRENCY.CURRENCY (id int,name varchar(255),area varchar(255),symbol varchar(255),timestamp bigint(13),date date,time time,spotin varchar(255),cashin varchar(255),spotout varchar(255),cashout varchar(255),middleprice varchar(255))', function (error, results, fileds) {
       if(error){
         reject(error);
       }else{
