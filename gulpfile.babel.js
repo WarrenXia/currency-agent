eb43bc25d155e5ec464fa59bcaf825f50057f797
xiashihua <xsh613@gmail.com>
2016-12-02 18:08:50 +0800

    add build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,29 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import nodemon from 'gulp-nodemon';
 
 gulp.task('babel', (cb) => {
   gulp.src('lib/*.js')
     .pipe(babel())
     .pipe(gulp.dest('app'));
-    // 提示build完成
-    cb();
-    console.log('🎉  Done');
+  // 提示build完成
+  cb();
+  console.log('🎉  Done');
+});
+
+gulp.task('run', () => {
+  nodemon({
+    script: './app/app.js',
+    ext: 'js',
+    ignore: ['app/'],
+    watch: ['lib/']
+  });
 });
 
 gulp.task('watch', () => {
   gulp.watch('lib/*.js', ['babel']);
 });
 
-gulp.task('default', ['babel', 'watch'], () => {
-
+gulp.task('build', ['babel'], () => {});
+gulp.task('default', ['babel', 'run', 'watch'], () => {
 });
